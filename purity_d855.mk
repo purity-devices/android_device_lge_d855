7aeb59f838676278e1879b6dd79eb55b96eeb20d
AndroidRul3z <ferrettidario@gmail.com>
2016-09-27 22:51:37 +0200

    Initial

--- purity_d855.mk
+++ purity_d855.mk
@@ -17,15 +17,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/purity/common.mk)
 
 # Inherit from d855 device
 $(call inherit-product, device/lge/d855/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d855
-PRODUCT_NAME := aosp_d855
+PRODUCT_NAME := purity_d855
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D855
 PRODUCT_MANUFACTURER := LGE
