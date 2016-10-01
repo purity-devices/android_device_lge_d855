6e4eb74670df726ead6ae8421ebdd380becb0056
AndroidRul3z <ferrettidario@gmail.com>
2016-10-01 17:55:54 +0200

    Update

--- purity_d855.mk
+++ purity_d855.mk
@@ -18,7 +18,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/purity/common.mk)
+$(call inherit-product, vendor/purity/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/purity/config/nfc_enhanced.mk)
 
 # Inherit from d855 device
 $(call inherit-product, device/lge/d855/device.mk)
