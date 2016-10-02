472ba7bfef61dc9c148015cafc8d6b4b9ebd57c4
AndroidRul3z <ferrettidario@gmail.com>
2016-10-02 15:26:28 +0200

    Add supersu bump

--- purity.mk
+++ purity.mk
@@ -8,6 +8,10 @@ $(call inherit-product, vendor/purity/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := purity_d855
 
+# SuperSU Bump
+PRODUCT_COPY_FILES += \
+   vendor/purity/prebuilt/common/etc/UPDATE-SuperSU-BUMP.zip:system/addon.d/UPDATE-SuperSU.zip \
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
